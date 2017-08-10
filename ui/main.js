f2d59747692455934669654ceea5771026e94bad
Piriyatharsika <piriya3012@gmail.com>
2017-08-10 07:48:33 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 //change the content of html
 
-var element = document.getElementById("main-text");
-element.innerHTML("New Value");
+var element = document.getElementById('main-text');
+element.innerHTML('New Value');
