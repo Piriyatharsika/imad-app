d86c15f2272e8c7b28538a97f3bbf8591ebaee48
Piriyatharsika <piriya3012@gmail.com>
2017-08-10 08:02:31 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,19 @@
 console.log('Loaded!');
 
-//change the content of html
+//change the content of html with id "main-text" in div
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+
+//move image graduate every 50 ms to the right
+
+var image = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    image.style.marginLeft = marginLeft + 'px';
+}
+image.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
