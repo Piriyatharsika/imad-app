d1d3469d7386edf08bfcb0552fb73eaf120134de
Piriyatharsika <piriya3012@gmail.com>
2017-08-10 08:03:06 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ element.innerHTML = 'New Value';
 var image = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     image.style.marginLeft = marginLeft + 'px';
 }
 image.onclick = function(){
