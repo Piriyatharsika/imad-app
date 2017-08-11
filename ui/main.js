6dc184fe45b20d8c3423954df68d7cbde9bc275d
Piriyatharsika <piriya3012@gmail.com>
2017-08-11 12:39:55 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,3 +43,23 @@ counterElement.onclick = function(){
     request.open('GET', 'http://piriya3012.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+
+//Submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //Make request to the server and send the name
+    
+    
+    //Capture a number of name and render all as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i = 0; i < names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = ul;
+};
