6d9b0304bab41becdfcee0fb798e021f5c712f90
Piriyatharsika <piriya3012@gmail.com>
2017-09-14 16:18:47 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.post('/login', function(req, res){
     
     pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
      if(err){
-         res.status(500).send(err.toString());
+         res.status(500).send(JSON.stringify(r.toString()));
      }else{
          if(result.rows.length === 0){
              res.status(403).send(JSON.stringify({"error":"username/password is invalid"}));
